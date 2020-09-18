c8c46276a53eab0d7c771341c31baace48328d27
pooja.patil <pooja.patil@nonstopio.com>
2020-09-18 15:40:03 +0530

    Added MockFireStoreInstance specific changes,added batch in firebase api file

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore_mocks/cloud_firestore_mocks.dart';
 
 SetOptions mergeOption = SetOptions(
   merge: true,
@@ -7,9 +8,13 @@ SetOptions mergeOption = SetOptions(
 class FirebaseApi {
   final String path;
   CollectionReference ref;
+  final bool isTest;
 
-  FirebaseApi(this.path) {
-    ref = FirebaseFirestore.instance.collection(path);
+  FirebaseApi(this.path, {this.isTest: false}) {
+    if (isTest)
+      ref = MockFirestoreInstance().collection(path);
+    else
+      ref = FirebaseFirestore.instance.collection(path);
   }
 
   Future<QuerySnapshot> getDataCollection() {
@@ -34,6 +39,13 @@ class FirebaseApi {
 
   Future<void> addDocument({String id, Map<String, dynamic> data}) {
     return ref.doc(id).set(data);
+  }
+
+  WriteBatch batch() {
+    if (isTest)
+      return MockFirestoreInstance().batch();
+    else
+      return FirebaseFirestore.instance.batch();
   }
 
   Future<void> updateDocument({String id, Map<String, dynamic> data}) {
