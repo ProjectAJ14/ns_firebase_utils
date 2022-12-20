d7fe922d8c0ffb7c6641a118e3d467907f2207d0
Ajay Kumar <ajay.kumar@nonstopio.com>
2022-12-20 17:39:18 +0530

    Added scripts

--- test/ns_firebase_utils_test.dart
+++ test/ns_firebase_utils_test.dart
@@ -1,1 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ns_firebase_utils/constants.dart';
 
+void main() {
+  // Test constants
+  test('defaultBool', () => expect(defaultBool, false));
+  test('defaultInt', () => expect(defaultInt, 0));
+  test('defaultDouble', () => expect(defaultDouble, 0.0));
+  test('defaultString', () => expect(defaultString, ''));
+  test('defaultMap', () => expect(defaultMap, {}));
+  test('defaultList', () => expect(defaultList, []));
+  test('defaultGeoPoint', () => expect(defaultGeoPoint, GeoPoint(0, 0)));
+}
